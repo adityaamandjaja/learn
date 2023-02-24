1d3fc518a65d8d246a6a69e43dcc9ec37d342ed4
Aditya <aditya@amandjaja.com>
2023-02-24 16:20:55 +0700

    init commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'LEARN',
+    'version': '1.0.0',
+    'category': 'Learn',
+    'sequence': 1,
+    'summary': '',
+    'description': """""",
+    'depends': ['web', 'stock', 'mrp'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
